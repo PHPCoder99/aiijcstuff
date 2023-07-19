03f8fe4b582cc93176e35a1b21771535b4e4dbc1
Zo PHPCoder99 <gergenkopzoya@gmail.com>
2023-07-19 13:43:43 +0700

    b

--- task1489.py
+++ task1489.py
@@ -16,7 +16,30 @@ for i in range(2, 9):
         B += str(i)
         A //= i
 
-if isPrime:
+if isPrime or A != 1:
     print("Лимитированная партия")
+elif B == "":
+    print("1")
 else:
     print(B)
+
+# def t1489(A):
+#     B = ""
+#
+#     isPrime = True
+#     if A < 2:
+#         isPrime = False
+#     else:
+#         for i in range(2, int(A ** 0.5) + 1):
+#             if A % i == 0:
+#                 isPrime = False
+#
+#     for i in range(2, 9):
+#         while A % i == 0:
+#             B += str(i)
+#             A //= i
+#
+#     if isPrime:
+#         return "x"
+#     else:
+#         return B
